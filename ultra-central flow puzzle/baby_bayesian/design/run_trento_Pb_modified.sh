764eb605ccbce2e1dff7baee2761cdf6d2c313f5
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-02-07 23:54:24 -0300

    Add run_trento_Pb_modified.sh script for Trento simulation

---
+++ ultra-central flow puzzle/baby_bayesian/design/run_trento_Pb_modified.sh
@@ -0,0 +1,9 @@
+trento \
+  Pb_beta2_015_beta3_000_beta4_003.hdf \
+  Pb_beta2_015_beta3_000_beta4_003.hdf \
+  --cross-section 6.4 \
+  --number-events 200000 \
+  --p 0.0 \
+  --fluctuation 1.2 \
+  --nucleon-width 0.5 \
+  --output trento.h5
