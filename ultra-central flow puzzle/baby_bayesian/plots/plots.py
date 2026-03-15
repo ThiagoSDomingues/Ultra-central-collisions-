112dc916eeecde60c3f009753443d0b710123fbc
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-03-15 01:21:08 -0300

    Update plots.py

--- ultra-central flow puzzle/baby_bayesian/plots/plots.py
+++ ultra-central flow puzzle/baby_bayesian/plots/plots.py
@@ -6,13 +6,48 @@ Making important plots for ultracentral collisions project.
 from compute_eccentricities import *
 from matplotlib import pyplot as plt
 from pbpb_run_trento import centrality bins
+from matplotlib import pyplot as plt
 
+# ============================
+# PLOTS VS CENTRALITY
+# ============================
 
-# plot 2-particle eccentricities individually. Centrality bins range from 0-90%. n = 2 and 3 overlaid 
-epsilon_n_2 vs centrality 
-plt.plot(centrality_bins, epsilon_2_2, )
-plt.plot(centrality_bins, epsilon_3_2, )
+plt.figure()
 
+# plot 2-particle eccentricities individually (RMS ecccentricities). Centrality bins range from 0-90%. n = 2,3, and 4 overlaid 
+plt.plot(centrality_bins, eps22, label=r'$\epsilon_2{2}$')
+plt.plot(centrality_bins, eps32, label=r'$\epsilon_3{2}$')
+plt.plot(centrality_bins, eps42, label=r'$\epsilon_4{2}$')
+
+plt.xlabel("Centrality (%)")
+plt.ylabel("Eccentricity")
+plt.legend()
+
+plt.savefig(WORKDIR/"ecc_vs_centrality.pdf")
+
+plt.figure()
+
+# Eccentricities ratios
+plt.plot(centrality_bins, ratio32, label=r'$\epsilon_3{2}/\epsilon_2{2}$')
+plt.plot(centrality_bins, ratio24, label=r'$\epsilon_2{2}/\epsilon_2{4}$')
+plt.plot(centrality_bins, ratio34, label=r'$\epsilon_3{2}/\epsilon_3{4}$')
+
+plt.xlabel("Centrality (%)")
+plt.legend()
+
+plt.savefig(WORKDIR/"ratios_vs_centrality.pdf")
+
+# ============================
+# PLOTS VS MODEL PARAMETERS
+# ============================
+
+#ratio
+
+#for i,name in enumerate(PARAM_NAMES):
+    
+#    plt.figure()
+    
+#    plt.scatter(design[:,i])    
 
 # plot eccentricities against parameters 
 plt.plot()
