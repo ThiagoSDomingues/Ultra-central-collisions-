be4d34f0702d2b6a9550f27cee689487c5ae2827
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-03-23 19:33:14 -0300

    Delete ultra-central flow puzzle/baby_bayesian/design/run_trento_Pb_modified.sh

--- ultra-central flow puzzle/baby_bayesian/design/run_trento_Pb_modified.sh
+++
@@ -1,9 +0,0 @@
-trento \
-  Pb_beta2_015_beta3_000_beta4_003.hdf \
-  Pb_beta2_015_beta3_000_beta4_003.hdf \
-  --cross-section 6.4 \
-  --number-events 200000 \
-  --p 0.0 \
-  --fluctuation 1.2 \
-  --nucleon-width 0.5 \
-  --output trento.h5
