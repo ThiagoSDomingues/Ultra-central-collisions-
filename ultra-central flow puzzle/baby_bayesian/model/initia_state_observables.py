fdef54ee80dac5a2a42b095787eb14ba20f32dd7
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-04-20 20:45:23 -0300

    Add initial state observables calculation script

---
+++ ultra-central flow puzzle/baby_bayesian/model/initia_state_observables.py
@@ -0,0 +1,22 @@
+import numpy as np
+# Normalized cumulants for the first design point
+
+arr = np.load("design_0000/trento_events.npy")
+
+# centrality
+mult = arr[:,3]
+rank = np.argsort(np.argsort(-mult))
+cent = rank / len(mult) * 100
+
+mask = (cent < 1)
+
+eps3 = arr[mask, 7]
+
+m2 = np.mean(eps3**2)
+m4 = np.mean(eps3**4)
+
+nc_2_4 = (m4 - 2*m2**2)/(m2**2)
+ratio_4_2 = -(nc_2_4)**(1/4)
+
+
+print(nc_2_4, ratio_4_2)
